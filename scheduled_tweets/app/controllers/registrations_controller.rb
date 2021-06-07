2d025d4c343da3581dd6785baff5233b200e7f75
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-06-07 20:03:56 +0530

    Signup errors handled

--- scheduled_tweets/app/controllers/registrations_controller.rb
+++ scheduled_tweets/app/controllers/registrations_controller.rb
@@ -4,9 +4,18 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        render plain: params[:user]
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            render :new
+        end
     end
 
+    private 
+    def user_params
+        params.require(:user).permit(:email,:password,:password_confirmation)
+    end
 
 end
 
