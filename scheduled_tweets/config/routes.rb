5e949cd057274ad7f3bc6a7834b3a886ad03f52d
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-06-08 12:24:42 +0530

    Logout created

--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'about', to:'about#index'
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
   root to:'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
