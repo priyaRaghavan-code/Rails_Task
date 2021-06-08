207e2fdd4f66a0a50d807e6ada9cf526618e5525
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-06-08 13:01:17 +0530

    Login form created

--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get 'about', to:'about#index'
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+
   delete 'logout', to: 'sessions#destroy'
   root to:'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
