5e506d3c7e0eca4273385065e131645a30ed27f1
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-06-23 17:56:08 +0530

    Updated devise

--- employeepolio/app/controllers/application_controller.rb
+++ employeepolio/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   add_flash_types :error, :success
+  def after_sign_in_path_for(resource)
+    new_employee_path
+  end
+
+  def after_sign_out_path_for(resource)
+    employees_path
+  end
+  
 end
